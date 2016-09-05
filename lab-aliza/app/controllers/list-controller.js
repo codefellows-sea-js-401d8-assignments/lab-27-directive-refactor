3ca237d38b45b5dca6774ae82f6051047818c824
Aliza Pilisuk <apilisuk@gmail.com>
2016-09-05 16:51:53 -0700

    cancel not working

--- lab-aliza/app/controllers/list-controller.js
+++ lab-aliza/app/controllers/list-controller.js
@@ -5,43 +5,53 @@ module.exports = (app) => {
 };
 
 function ListController($log, $http) {
+
+  let baseUrl = `${__API_URL__}/api/list`;
+  let config = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+
   this.lists = [];
   this.getAllLists = function() {
-    $http.get(this.baseUrl, this.config)
+    $http.get(baseUrl, config)
       .then(res => {
+        $log.log('Success: ', res.data);
         this.lists = res.data;
       }, err => {
-        $log.error('error!', err);
+        $log.error('Error: ', err);
       });
   };
 
   this.destroyList = function(list) {
     $log.debug('listCtrl.destroyList');
-    $http.delete(this.baseUrl + '/' + list._id, this.config)
+    $http.delete(baseUrl + '/' + list._id, config)
       .then(res => {
-        $log.log('success!', res.data);
+        $log.log('Success: ', res.data);
         this.lists.splice(this.lists.indexOf(list), 1);
       }, err => {
-        $log.error('error!', err);
+        $log.error('Error: ', err);
       });
   };
 
   this.updateList = function(list) {
-    $http.put(this.baseUrl + '/' + list._id, list, this.config)
+    $http.put(baseUrl + '/' + list._id, list, config)
       .then(res => {
-        $log.log('success!', res.data);
+        $log.log('Success: ', res.data);
         list.editing = false;
       }, err => {
-        $log.error('error!', err);
+        $log.error('Error: ', err);
       });
   };
 
   this.createList = function(list){
-    $http.post(this.baseUrl, list, this.config)
+    $http.post(baseUrl, list, config)
       .then(res => {
-        $log.log('Success!', res.data);
+        $log.log('Success: ', res.data);
         this.lists.push(res.data);
-        $log.log('this.lists', this.lists);
+        $log.log('this.lists: ', this.lists);
       })
       .catch(err => {
         $log.log('Error: ', err);
--- lab-aliza/app/index.js
+++ lab-aliza/app/index.js
@@ -6,15 +6,5 @@ require('./scss/base.scss');
 const angular = require('angular');
 let listApp = angular.module('listApp', []);
 
-listApp.run(['$rootScope', ($rs) => {
-  $rs.noteListUrl = `${__API_URL__}/api/list`;
-  $rs.noteHttpConfig = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept-Content': 'application/json'
-    }
-  };
-}]);
-
 require('./components')(listApp);
 require('./controllers')(listApp);
