cbeb9cc488f08b6663fdd459c4240bd7225122cd
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-09-05 12:48:36 -0700

    added getLists function to components/list/list_controller.js. also added dummy button to list-template.html

--- lab-james/app/components/list/list_controller.js
+++ lab-james/app/components/list/list_controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('ListController', function() {
-    console.log('list controller');
-  });
+  app.controller('ListController', ['$log', '$http', function($log, $http) {
+    $log.log('list controller');
+    this.baseUrl = `${__API_URL__}/api/list`;
+    this.config = {
+      'Accept': 'Application/json',
+      'Content-Type': 'Application/json'
+    };
+
+    this.getLists = function() {
+      $log.log('getLists function');
+      $http.get(this.baseUrl)
+        .then((res) => {
+          $log.log('res data: ' + res.data);
+        })
+        .catch((err) => {
+          $log.log('Error in getting lists: ' + err);
+        });
+    };
+  }]);
 };
