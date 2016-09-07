9649e4d3b919914724fe846989f2ed9ac55c574a
Aliza Pilisuk <apilisuk@gmail.com>
2016-09-06 20:35:31 -0700

    set up frontend, backend, and readme. Need to add routes and html for front-end

---
+++ lab-aliza/back-end/model/note.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+Promise.promisifyAll(mongoose);
+
+let noteSchema = Schema({
+  name: {type: String, required: true},
+  listId: {type: Schema.Types.ObjectId, required: true},
+  content: {type: String, require: true}
+});
+
+module.exports = mongoose.model('Note', noteSchema);
