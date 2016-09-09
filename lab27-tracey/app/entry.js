0aa196e2f938a0bae7520a3c95b89fd6c25b14d8
Tracey <radcliffe.tracey@gmail.com>
2016-09-08 20:50:53 -0700

    edited routes and html

--- lab27-tracey/app/entry.js
+++ lab27-tracey/app/entry.js
@@ -4,10 +4,14 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-let lab27 = angular.module('lab27', []);
+let lab27 = angular.module('lab27', [require('angular-route')]);
+
+require('./component')(lab27);
+require('./controller')(lab27);
 
 lab27.run(['$rootScope', ($rs) =>{
-  $rs.noteListUrl = `${__API_URL__}/api/list`;
+  $rs.baseUrl = `${__API_URL__}`;
+  $rs.noteListUrl = $rs.baseUrl + '/api/list';
   $rs.noteHttpConfig = {
     headers: {
       'Content-Type': 'application/json',
@@ -16,4 +20,18 @@ lab27.run(['$rootScope', ($rs) =>{
   };
 }]);
 
-require('./component')(lab27);
+lab27.config(['$routeProvider', ($rp)=>{
+  $rp
+    .when('/notes', {
+      template: require('./html/notes.html')
+    })
+    .when('/signup', {
+      template: require('./html/signup.html')
+    })
+    .when('./signin', {
+      template: require('./html/signin.html')
+    })
+    .otherwise({
+      redirectTo: 'notes'
+    });
+}]);
