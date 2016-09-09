38e7d5cc37b7ce45f09a681bb3f1c71e83a9f254
Tracey <radcliffe.tracey@gmail.com>
2016-09-08 17:24:35 -0700

    changed tests but still not working

--- lab27-tracey/test/list-test/list-test.js
+++ lab27-tracey/test/list-test/list-test.js
@@ -5,21 +5,30 @@ let testApp = angular.module('testApp', []);
 
 require('../../app/component/list')(testApp);
 
-describe('testing list compnents', function(){
+describe('testing list compnents', ()=> {
   beforeEach(angular.mock.module('testApp'));
-  beforeEach(angular.mock.inject(function($compile, $rootScope){
-    this.compile = $compile;
-    this.scope = $rootScope.$new();
-    this.scope.baseUrl = 'http://localhost:3000/api/list';
-    this.scope.headers = {
-      'Content-Type': 'application/json',
-      'Accept-Content': 'application/json'
-    };
+  beforeEach(angular.mock.inject(($controller, $httpBackend)=>{
+    this.listCtrl = new $controller('ListController');
+    this.$httpBackend = $httpBackend;
   }));
 
-  it('should display some lists', function(){
-    let list = this.compile('<div list-directive config="headers" base-url="{{baseUrl}}"></div>')(this.scope);
-    this.scope.$digest();
-    expect(list.find('').text()).toBe('');
+  it('testing createList', () => {
+    let url = 'http://localhost:3000/api/list';
+    let requestData = {name: 'example name'};
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    console.log(this.$httpBackend);
+    this.$httpBackend.expectPOST(url, requestData, headers)
+    .respond(200, {
+      name: 'example name',
+      _id: '1234',
+      __v: 0,
+      notes: []
+    });
+
+    this.listCtrl.createList(requestData);
+    this.$httpBackend.flush();
   });
 });
